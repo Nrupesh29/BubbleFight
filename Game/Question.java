1584efe8861cbd0e4d137eee5a1702cfa1a727ae
ravibgupta <ravi.gupta@sjsu.edu>
2016-11-13 17:09:48 -0800

    Add files via upload

--- Game/Question.java
+++ Game/Question.java
@@ -7,24 +7,17 @@
 public class Question  
 {
     // instance variables - replace the example below with your own
-    private int x;
+    private String question;
+    private Answer answer;
 
     /**
      * Constructor for objects of class Question
      */
-    public Question()
+    public Question(String q, Answer a)
     {
+        question = q;
+        answer = a;
     }
+    
 
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
 }

bb8b9327d99a7b023fb6278ac18addeabe7fdb42
ravibgupta <ravi.gupta@sjsu.edu>
2016-11-20 23:48:54 -0800

    The class implementing the answerlist logic

---
+++ Game/AnswerList.java
@@ -0,0 +1,12 @@
+public class AnswerList{
+
+String[] ans = {"1010","1011", "1011","1011", "1111", "1111"};
+
+int b;
+String[] strategy
+
+public void AnswerList(){
+int c = Math.random();
+} 
+
+}

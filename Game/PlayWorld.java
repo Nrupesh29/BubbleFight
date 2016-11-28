f47a12f8d961c27ad6e7ee6f6e971e4a81ddffda
nam <naph47@gmail.com>
2016-11-27 21:34:14 -0800

    fix game state

--- Game/PlayWorld.java
+++ Game/PlayWorld.java
@@ -43,12 +43,5 @@ public class PlayWorld extends World
         gamesystem.startGame();
     }
 
-    /**
-     * Prepare the world for the start of the program.
-     * That is: create the initial objects and add them to the world.
-     */
-    private void prepare()
-    {
-
-    }
+   
 }

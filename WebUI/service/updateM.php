f89970d569cdc36ddb9f437189ff1035fe3b5e17
nam <naph47@gmail.com>
2016-12-09 18:11:42 -0800

    updateM

--- WebUI/service/updateM.php
+++ WebUI/service/updateM.php
@@ -1,10 +1,9 @@
 <?php
-extract($_POST);
+extract($_GET);
 $curl = curl_init();
 curl_setopt($curl, CURLOPT_URL, "http://sample-env.xtfzxnrydy.us-west-1.elasticbeanstalk.com/api/match/" . $id);
 curl_setopt($ch, CURLOPT_PUTFIELDS, "winnerId=" . $w . "&matchScore=" . $s);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
-
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
 $result = curl_exec($curl);

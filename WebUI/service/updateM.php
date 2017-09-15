f334635ad956165c19fa0c8136e5ca930de949ea
nam <naph47@gmail.com>
2017-09-15 13:58:43 +0700

    update

--- WebUI/service/updateM.php
+++ WebUI/service/updateM.php
@@ -3,13 +3,14 @@ extract($_POST);
 print_r($_POST);
 $curl = curl_init();
 curl_setopt($curl, CURLOPT_URL, "http://sample-env.xtfzxnrydy.us-west-1.elasticbeanstalk.com/api/match/" . $id);
-//curl_setopt($ch, CURLOPT_PUTFIELDS, "winnerId=" . $w . "&matchScore=" . $s);
-curl_setopt($curl, CURLOPT_PUTFIELDS, array('winnerId' => $winnerId, 'score' => $score, 'playerOneId' => $playerOneId, 'playerTwoId' => $playerTwoId, 'tournamentId' => $tournamentId, 'levelId' => $levelId, 'matchDate' => $matchDate));
+curl_setopt($ch, CURLOPT_POSTFIELDS, "winnerId=" . $winnerId . "&matchScore=" . $score);
+//curl_setopt($curl, CURLOPT_PUTFIELDS, array('winnerId' => $winnerId, 'score' => $score, 'playerOneId' => $playerOneId, 'playerTwoId' => $playerTwoId, 'tournamentId' => $tournamentId, 'levelId' => $levelId, 'matchDate' => $matchDate));
 curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
-//curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
 $result = curl_exec($curl);
 curl_close($curl);
 
-//print($result);
+print($result);
 ?>
